88890d6729f00659285b689238ff0694890d7235
K-Nash55 <ken.nakanishi02@gmail.com>
2026-08-11 02:13:09 +0900

    feat: スーパーアドミン専用ログイン機能を追加

---
+++ js/super-admin-guard.js
@@ -0,0 +1,10 @@
+﻿// スーパーアドミン画面(super_dashboard.html / superadmin.html)共通の入場チェック
+(async function () {
+  var ALLOWED_EMAIL = 'ken.nakanishi02@gmail.com';
+  var { data } = await supabase.auth.getSession();
+  var session = data && data.session;
+  if (!session || !session.user || session.user.email !== ALLOWED_EMAIL) {
+    var here = encodeURIComponent(location.pathname.split('/').pop());
+    window.location.replace('super-login.html?redirect=' + here);
+  }
+})();
